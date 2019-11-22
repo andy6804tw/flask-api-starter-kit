48f17fa7368c2036d70d9c4bf75bcd25dd069883
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-11-22 12:31:32 +0800

    add upload router

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
+import config
 from flask import Flask,request,render_template
 from flask_cors import CORS
-import config
+# from app.controllers.model import model 
+from app.controllers.upload import upload, upFile
+from flask_uploads import configure_uploads
 
-app = Flask(__name__, static_folder='public')
+app=Flask(__name__)
 CORS(app)
-app.config.from_object(config)
+app.config.from_object(config) # 由config.py管理環境變數
+configure_uploads(app, upFile)
 
+
+app.register_blueprint(upload, url_prefix='/upload')
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 DEBUG = True
 PORT=5000
 ENV='development'
+SECRET_KEY = 'files'
+UPLOADED_UPFILE_DEST = 'app/static'
