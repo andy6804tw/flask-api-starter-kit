eb2fbcd0e55836a68f28ff4ba4fcb64da6b41bd8
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-27 19:06:52 +0800

    un log

--- app_run.py
+++ app_run.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
   # print(app.url_map)
   @app.route('/')
   def index():
-    return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
+    # return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
+    return'sadsadsad'
   # app.run(port=config.PORT)
   app.run(debug=True)
