27470da729c7c43919d74f0b2304f67fd978ab36
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-27 19:02:29 +0800

    add apprun

--- app_run.py
+++ app_run.py
@@ -1,7 +1,7 @@
-from app import app
+# from app import app
 import config
 if __name__ == '__main__':
-  print(app.url_map)
+  # print(app.url_map)
   @app.route('/')
   def index():
     return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
