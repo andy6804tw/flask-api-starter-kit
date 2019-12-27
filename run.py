23d78ec683202ec018fff728432adc94ad2f92a2
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-27 19:52:57 +0800

    add main index

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from app import app
 import config
 
 @app.route('/')
-  def index():
-    return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
+def index():
+  return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
 
 if __name__ == '__main__':
   # print(app.url_map)
