6f46a58f535120041a6379fd9470d78ccbc64ab8
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-12-27 19:48:39 +0800

    add main index

--- run.py
+++ run.py
@@ -3,12 +3,11 @@ from flask import *
 
 app = Flask(__name__)
 import config
-
 @app.route('/')
-  def index():
-    # return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
-    return'sadsadsad'
-    
+def index():
+  # return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
+  return'sadsadsad'
+
 if __name__ == '__main__':
   # print(app.url_map)
   
